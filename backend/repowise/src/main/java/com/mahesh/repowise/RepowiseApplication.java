95fab55a86de057273959d8a464af9c02db84baf
Mahesh <maheshmishra1069@gmail.com>
2026-06-18 09:46:07 +0530

    Backend project stepup

---
+++ backend/repowise/src/main/java/com/mahesh/repowise/RepowiseApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.repowise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepowiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepowiseApplication.class, args);
+	}
+
+}
---
+++ backend/repowise/src/test/java/com/mahesh/repowise/RepowiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.repowise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepowiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
